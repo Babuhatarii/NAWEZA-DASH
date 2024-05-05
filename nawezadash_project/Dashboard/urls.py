9d5f114f3849d824ade734333e748056b6357661
Babuhatarii <ochegoharyon@gmail.com>
2024-05-05 11:54:57 +0300

    more views

--- nawezadash_project/Dashboard/urls.py
+++ nawezadash_project/Dashboard/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import activity_detail
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:activity_id>/", views.activity_detail, name='activity_detail'),
 ]
--- nawezadash_project/Dashboard/views.py
+++ nawezadash_project/Dashboard/views.py
@@ -1,5 +1,20 @@
 from django.http import HttpResponse
-
+from .models import Activity, Enrollment, Progress
 
 def index(request):
     return HttpResponse("Hello, world. You're at Naweza-Dash.")
+
+def activity_detail(request, activity_id):
+    """
+    View function to display details of a specific activity.
+    """
+    activities = Activity.objects.all()
+    """
+    Generate a response containing the list of all activities
+    """
+    response = "\n".join([Activity.name for Activity in activities])
+
+    """Return the response"""
+    return HttpResponse(response)
+
+    
