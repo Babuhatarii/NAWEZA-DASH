57bddf8e5d122ab63030fa4ceb5ac7e60127039c
Babuhatarii <ochegoharyon@gmail.com>
2024-05-08 21:29:51 +0300

    sign in

--- nawezadash_project/Dashboard/urls.py
+++ nawezadash_project/Dashboard/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-from .views import activity_detail, landing_page  # Import views
+from django.urls import path, re_path
+from .views import activity_detail, landing_page, signin_page # Import views
 
 urlpatterns = [
     path('', landing_page, name='landing_page'),  # Root URL
+    path('signin/', signin_page, name='signin_page'),  # Signin page URL
     path('<int:activity_id>/', activity_detail, name='activity_detail'),  # Dynamic URL
 ]
--- nawezadash_project/Dashboard/views.py
+++ nawezadash_project/Dashboard/views.py
@@ -1,12 +1,23 @@
 from django.http import HttpResponse
 from .models import Activity, Enrollment, Progress
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def landing_page(request):
     """
     View function to render the landing page.
     """
     return render(request, 'landing.html')
+
+def signin_page(request):
+    """
+    view function o render the sign in page.
+    """
+    return render(request, 'sign_in.html')
+
+def mydashboard(request):
+    #my dashboard view
+    return render(request, 'mydashboard.html')
+
 
 def activity_detail(request, activity_id):
     """
